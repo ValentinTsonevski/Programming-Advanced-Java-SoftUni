5c5e999a827c134ccde339540bdbfdb3eddbfa0e
Valentin Tsonevski <tsonevski99@gmail.com>
2022-08-18 23:41:36 +0100

    Add files via upload

---
+++ Functional Programming - Lab/src/SortEvenNumbers.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SortEvenNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> evenNumbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt).filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        String output = evenNumbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(output);
+
+        System.out.println(evenNumbers.stream().sorted().map(String::valueOf).collect(Collectors.joining(", ")));
+
+
+    }
+}
